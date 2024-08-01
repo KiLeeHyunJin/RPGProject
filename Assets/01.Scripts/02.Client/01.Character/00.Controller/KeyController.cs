b4fc7c951300110d9980b92a75299bf70aa61ff2
NoteBook <kistudentmeta5@gmail.com>
2024-08-02 01:10:34 +0900

    Feat : Key Method Add & Remove

--- Assets/01.Scripts/02.Client/01.Character/00.Controller/KeyController.cs
+++ Assets/01.Scripts/02.Client/01.Character/00.Controller/KeyController.cs
@@ -1,87 +1,78 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
+using UnityEngine;
 using UnityEngine.InputSystem;
 using static Define;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
 
+[Serializable]
 public partial class KeyController
 {
-    readonly string keyMapName;
-    readonly List<bool> usedKey;
+    const string KeyMapName = "DynamicKeySetting";
+    const string KeyBindingFirstName = "<Keyboard>/";
+    readonly UserCharacterController controller;
+    readonly InputActionMap actionMap;
+    readonly Action<InputAction.CallbackContext>[] keyActionArray;
 
-    InputActionMap actionMap;
-    UserCharacterController controller;
+    [SerializeField] List<int> keyDataServer;
+
     public List<KeyData> keyDataSet;
-    public List<int> keyDataServer;
-    bool this[Key key]
-    {
-        get { return usedKey[(int)key]; }
-        set { usedKey[(int)key] = value; }
-    }
+    List<KeyData> KeyDataList { get { return keyDataSet; } }
+    bool this[Key key]  {   get { return keyDataSet[(int)key].state; }  }
 
     public KeyController(InputActionAsset inputActions, UserCharacterController userController)
     {
-        keyMapName = "DynamicKeySetting";
         controller = userController;
         Key[] keysArray = Utils.GetEnumArray<Key>();
-        int endNum = (int)keysArray[Define.EndIndex];
+        int endNum = keysArray.Length;
+
+        actionMap = inputActions.FindActionMap(KeyMapName);
+        actionMap ??= inputActions.AddActionMap(KeyMapName);
 
         keyDataSet = new(endNum);
-        usedKey = new(endNum);
         keyDataServer = new(endNum);
-
-        Init(inputActions, keysArray);
+        keyActionArray = new Action<InputAction.CallbackContext>[endNum];
+        Init(keysArray);
     }
 
-    void Init(InputActionAsset inputActions, Key[] keysArray)
+    void Init( Key[] keysArray)
     {
-        //¿Œ«≤Ω√Ω∫≈€¿ª ∫Ò»∞º∫»≠«’¥œ¥Ÿ.
-        inputActions.Disable();
-
-        //æ◊º« ∏ ¿ª ª˝º∫«’¥œ¥Ÿ. 
-        actionMap = inputActions.FindActionMap(keyMapName);
-        if (actionMap == null)
-            actionMap = inputActions.AddActionMap(keyMapName);
-
-        StringBuilder stringBuilder = new();
         InputAction inputAction;
         for (int i = 0; i < keysArray.Length; i++)
         {
-            stringBuilder.Append(keysArray[i].ToString());
-            inputAction = actionMap.FindAction(stringBuilder.ToString());
-            if (inputAction == null)
+            keyDataServer.Add(0);
+            keyActionArray[i] = null;
+            KeyDataList.Add(new() { key = keysArray[i] });
+            inputAction = actionMap.FindAction(keysArray[i].ToString());
+            if (inputAction != null)
             {
-                actionMap.AddAction(keysArray[i].ToString(), InputActionType.Button, binding: $"<Keyboard>/{keysArray[i]}").performed += (cal) => { TestKey(); };
+                inputAction.RemoveAction();
             }
-            else
-                inputAction.Reset();
-
-            stringBuilder.Clear();
+            actionMap.AddAction(keysArray[i].ToString(), binding: $"{KeyBindingFirstName}{keysArray[i]}");
         }
-        //¿Œ«≤Ω√Ω∫≈€¿ª »∞º∫»≠«’¥œ¥Ÿ.
-        inputActions.Enable();
+        //Ïù∏ÌíãÏãúÏä§ÌÖúÏùÑ ÌôúÏÑ±ÌôîÌï©ÎãàÎã§.
+        //inputActions.Enable();
     }
     public void TestKey()
     {
         UnityEngine.Message.Log("sdad");
     }
+
     public void ResetKey()
     {
         Key[] keysArray = Utils.GetEnumArray<Key>();
-        StringBuilder stringBuilder = new();
         InputAction inputAction;
         actionMap.Disable();
         for (int i = 0; i < keysArray.Length; i++)
         {
-            stringBuilder.Append(keysArray[i].ToString());
-            inputAction = actionMap.FindAction(stringBuilder.ToString());
+            inputAction = actionMap.FindAction(keysArray[i].ToString());
             if (inputAction != null)
             {
-                inputAction.Reset();
-                inputAction.RemoveAction();
+                RemoveKey(keysArray[i]);
             }
-
-            stringBuilder.Clear();
         }
         actionMap.Enable();
     }
@@ -95,86 +86,132 @@ public partial class KeyController
                 continue;
             
             KeyData keyParseData = ParseKeySlotData(keyArray[i]);
-            this[keyParseData.key] = true;
-            keyDataSet[(int)keyParseData.key] = keyParseData;
-            SetKeyMapping(keyParseData.slotType,keyParseData.idx, actionMap.FindAction(keyParseData.key.ToString()));
+            if(keyParseData.state)
+                SetKeyMapping(keyParseData, actionMap.FindAction(keyParseData.key.ToString()));
         }
     }
 
 
-    void SetKeyMapping(QuickSlotType slotType, int idx, InputAction inputAction)
+    void SetKeyMapping(KeyData keyData, InputAction inputAction)
     {
-        switch (slotType)
+        KeyDataList[(int)keyData.key] = keyData;
+        Action<InputAction.CallbackContext> action = keyData.slotType switch
         {
-            case QuickSlotType.Default: //±‚∫ª ¡¶∞¯ Ω∫≈≥
-                break;
-            case QuickSlotType.Item: //æ∆¿Ã≈€ ªÁøÎ
-                inputAction.Reset();
-                inputAction.performed += (callBackt)=> controller.Inventory.UseItem(ItemType.Consume, idx);
-                break;
-            case QuickSlotType.Skill: //Ω∫≈≥ ªÁøÎ
-                break;
-        }
+            QuickSlotType.Default => null,
+            QuickSlotType.Item => (callBackt) => controller.Inventory.UseItem(ItemType.Consume, keyData.idx),
+            QuickSlotType.Skill => null,
+            _ => null,
+        };
+
+        if (action == null)
+            return;
+
+        inputAction.performed += action;
+        keyActionArray[(int)keyData.key] = action;
     }
 
+
+    public void AddKey(Key keyCode, Action action,Define.QuickSlotType quickSlotType, int idx , InputActionType inputType = InputActionType.Button)
+    {
+        if (this[keyCode] == true)
+            RemoveKey(keyCode);
+
+
+        actionMap.Disable();
+        InputAction keyAction = actionMap.FindAction(keyCode.ToString());
+        if(keyAction != null)
+        {
+            keyAction.performed -= keyActionArray[(int)keyCode];
+        }
+        else
+        {
+            keyAction = actionMap.AddAction(keyCode.ToString(), binding: $"{KeyBindingFirstName}{keyCode}");
+        }
+        SetKeyDataServer(keyCode, quickSlotType, idx);
+        void inputAction(InputAction.CallbackContext call) { action?.Invoke(); }
+        keyActionArray[(int)keyCode] = inputAction;
+        keyAction.performed += inputAction;
+
+        actionMap.Enable();
+    }
 
     public void RemoveKey(Key keyCode)
     {
         keyDataServer[(int)keyCode] = 0;
-        this[keyCode] = false;
-
+        KeyDataList[(int)keyCode].Remove();
+        InputAction keyAction = actionMap.FindAction(keyCode.ToString());
+        if (keyAction != null)
+        {
+            actionMap.Disable();
+            Action<InputAction.CallbackContext> action = keyActionArray[(int)keyCode];
+            if (action != null)
+            {
+                keyAction.performed -= action;
+            }
+            else
+            {
+                keyAction.RemoveAction();
+                actionMap.AddAction(keyCode.ToString(), binding: $"{KeyBindingFirstName}{keyCode}");
+            }
+            actionMap.Enable();
+        }
     }
 
-    public void AddKey(Key keyCode, Action action,Define.QuickSlotType quickSlotType, int idx , InputActionType inputType = InputActionType.Button)
-    {
-        SetKeyDataServer(keyCode, quickSlotType, idx);
-
-        if (this[keyCode] == true)
-        {
-            GetKeyAction(keyCode).Reset();
-        }
-        else
-        {
-
-        }
-    }
 
 
     InputAction GetKeyAction(Key keyCode)
     {
         InputAction action = actionMap.FindAction(keyCode.ToString());
-        if (action == null)
-            action = actionMap.AddAction(keyCode.ToString(), InputActionType.Button);
+        action ??= actionMap.AddAction(keyCode.ToString(), InputActionType.Button, binding: $"{KeyBindingFirstName}{keyCode}");
         return action;
     }
 
 
 
-    void SetKeyDataServer(Key keyCode, Define.QuickSlotType quickSlotType, int idx)
+    void SetKeyDataServer(Key keyCode, Define.QuickSlotType quickSlotType, int idx, bool state = true)
     {
         int setServerData = default;
         setServerData |= ((int)keyCode).Shift(DataDefine.IntSize.One);
         setServerData |= ((int)quickSlotType).Shift(DataDefine.IntSize.One);
         setServerData |= idx.Shift(DataDefine.IntSize.Two);
+        setServerData |= state ? int.MaxValue.Shift(DataDefine.IntSize.Four) : 0;
+        KeyDataList[(int)keyCode].Add(keyCode, quickSlotType, idx);
         keyDataServer[(int)keyCode] = setServerData;
     }
 
     KeyData ParseKeySlotData(int value)
     {
         int setServerData = value;
-        KeyData keyData = new();
-        keyData.key = (Key)setServerData.ExtractByte(DataDefine.IntSize.One);
-        keyData.slotType = (QuickSlotType)setServerData.ExtractByte(DataDefine.IntSize.Two);
-        keyData.idx = setServerData.ExtractByte(DataDefine.IntSize.Three);
+        KeyData keyData = new()
+        {
+            key = (Key)setServerData.ExtractByte(DataDefine.IntSize.One),
+            slotType = (QuickSlotType)setServerData.ExtractByte(DataDefine.IntSize.Two),
+            idx = setServerData.ExtractByte(DataDefine.IntSize.Three),
+            state = setServerData.ExtractByte(DataDefine.IntSize.Four) > 0 
+        };
         return keyData;
     }
-
-    public struct KeyData
+    [Serializable]
+    public class KeyData
     {
         public Key key;
         public QuickSlotType slotType;
         public int idx;
+        public bool state;
+        public void Remove()
+        {
+            idx = -1;
+            state = false;
+        }
+        public void Add(Key _key, QuickSlotType _slotType, int _idx)
+        {
+            key = _key;
+            slotType = _slotType;
+            idx = _idx;
+            state = true;
+        }
     }
+
 }
 
 
